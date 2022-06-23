f27a36f7f815e431e000d0d80fa967956a1a0179
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-06-23 12:44:43 +0430

    Fix linters

--- JS/popup-content.js
+++ JS/popup-content.js
@@ -1,4 +1,4 @@
-/*eslint linebreak-style: ["error", "windows"]*/
+/*eslint linebreak-style: ['error', 'windows']*/
 const allContent = [
     {
         name: 'Tonic',
@@ -45,124 +45,124 @@ const allContent = [
 
 ];
 
-const modal0 = document.querySelector(".modal");
+const modal0 = document.querySelector('.modal');
 
-modal0.insertAdjacentHTML("afterbegin", `
-    <div class="modal-header">
-        <h2 class="title">${allContent[0].name}</h2>
-        <button data-close-button type="button" class="close-button">&times;</button>
+modal0.insertAdjacentHTML('afterbegin', `
+    <div class='modal-header'>
+        <h2 class='title'>${allContent[0].name}</h2>
+        <button data-close-button type='button' class='close-button'>&times;</button>
     </div>
-    <ul class="sub-title">
+    <ul class='sub-title'>
         <li>${allContent[0].work[0]}</li>
         <li>${allContent[0].work[1]}</li>
         <li>${allContent[0].work[2]}</li>
     </ul>
-    <div class="modal-photo" id="photoP">
+    <div class='modal-photo' id='photoP'>
     </div>
-    <section class="modal-body">
-        <p class="modal-body1">${allContent[0].content}</p>
-        <div class="modal-body2">
-            <ul class="category">
-                <li><a href="index.html">${allContent[0].languages[0]}</a></li>
-                <li><a href="index.html">${allContent[0].languages[1]}</a></li>
-                <li><a href="index.html">${allContent[0].languages[2]}</a></li>
+    <section class='modal-body'>
+        <p class='modal-body1'>${allContent[0].content}</p>
+        <div class='modal-body2'>
+            <ul class='category'>
+                <li><a href='index.html'>${allContent[0].languages[0]}</a></li>
+                <li><a href='index.html'>${allContent[0].languages[1]}</a></li>
+                <li><a href='index.html'>${allContent[0].languages[2]}</a></li>
             </ul>
-            <div class="extra-buttons">  
-            <div class="project"><p>See Live</p><img src="image/sm1.png"></div>
-            <div class="project"><p>See Source</p><img src="image/sm2.png"></div>
+            <div class='extra-buttons'>  
+            <div class='project'><p>See Live</p><img src='image/sm1.png'></div>
+            <div class='project'><p>See Source</p><img src='image/sm2.png'></div>
             </div>
         </div>
     </section> 
 
 `);
 
-const modal1 = document.querySelector("#");
+const modal1 = document.querySelector('#');
 
-modal1.insertAdjacentHTML("afterbegin", `
-    <div class="modal-header">
-        <h2 class="title">${allContent[0].name}</h2>
-        <button data-close-button type="button" class="close-button">&times;</button>
+modal1.insertAdjacentHTML('afterbegin', `
+    <div class='modal-header'>
+        <h2 class='title'>${allContent[0].name}</h2>
+        <button data-close-button type='button' class='close-button'>&times;</button>
     </div>
-    <ul class="sub-title">
+    <ul class='sub-title'>
         <li>${allContent[0].work[0]}</li>
         <li>${allContent[0].work[1]}</li>
         <li>${allContent[0].work[2]}</li>
     </ul>
-    <div class="modal-photo" id="photoP">
+    <div class='modal-photo' id='photoP'>
     </div>
-    <section class="modal-body">
-        <p class="modal-body1">${allContent[0].content}</p>
-        <div class="modal-body2">
-            <ul class="category">
-                <li><a href="index.html">${allContent[0].languages[0]}</a></li>
-                <li><a href="index.html">${allContent[0].languages[1]}</a></li>
-                <li><a href="index.html">${allContent[0].languages[2]}</a></li>
+    <section class='modal-body'>
+        <p class='modal-body1'>${allContent[0].content}</p>
+        <div class='modal-body2'>
+            <ul class='category'>
+                <li><a href='index.html'>${allContent[0].languages[0]}</a></li>
+                <li><a href='index.html'>${allContent[0].languages[1]}</a></li>
+                <li><a href='index.html'>${allContent[0].languages[2]}</a></li>
             </ul>
-            <div class="extra-buttons">  
-            <div class="project"><p>See Live</p><img src="image/sm1.png"></div>
-            <div class="project"><p>See Source</p><img src="image/sm2.png"></div>
+            <div class='extra-buttons'>  
+            <div class='project'><p>See Live</p><img src='image/sm1.png'></div>
+            <div class='project'><p>See Source</p><img src='image/sm2.png'></div>
             </div>
         </div>
     </section> 
 
 `);
 
-const modal2 = document.querySelector("#");
+const modal2 = document.querySelector('#');
 
-modal2.insertAdjacentHTML("afterbegin", `
-    <div class="modal-header">
-        <h2 class="title">${allContent[0].name}</h2>
-        <button data-close-button type="button" class="close-button">&times;</button>
+modal2.insertAdjacentHTML('afterbegin', `
+    <div class='modal-header'>
+        <h2 class='title'>${allContent[0].name}</h2>
+        <button data-close-button type='button' class='close-button'>&times;</button>
     </div>
-    <ul class="sub-title">
+    <ul class='sub-title'>
         <li>${allContent[1].work[0]}</li>
         <li>${allContent[1].work[1]}</li>
         <li>${allContent[1].work[2]}</li>
     </ul>
-    <div class="modal-photo" id="photoP">
+    <div class='modal-photo' id='photoP'>
     </div>
-    <section class="modal-body">
-        <p class="modal-body1">${allContent[0].content}</p>
-        <div class="modal-body2">
-            <ul class="category">
-                <li><a href="index.html">${allContent[2].languages[0]}</a></li>
-                <li><a href="index.html">${allContent[2].languages[1]}</a></li>
-                <li><a href="index.html">${allContent[2].languages[2]}</a></li>
+    <section class='modal-body'>
+        <p class='modal-body1'>${allContent[0].content}</p>
+        <div class='modal-body2'>
+            <ul class='category'>
+                <li><a href='index.html'>${allContent[2].languages[0]}</a></li>
+                <li><a href='index.html'>${allContent[2].languages[1]}</a></li>
+                <li><a href='index.html'>${allContent[2].languages[2]}</a></li>
             </ul>
-            <div class="extra-buttons">  
-            <div class="project"><p>See Live</p><img src="image/sm1.png"></div>
-            <div class="project"><p>See Source</p><img src="image/sm2.png"></div>
+            <div class='extra-buttons'>  
+            <div class='project'><p>See Live</p><img src='image/sm1.png'></div>
+            <div class='project'><p>See Source</p><img src='image/sm2.png'></div>
             </div>
         </div>
     </section> 
 
 `);
 
-const modal3 = document.querySelector("#");
+const modal3 = document.querySelector('#');
 
-modal3.insertAdjacentHTML("afterbegin", `
-    <div class="modal-header">
-        <h2 class="title">${allContent[0].name}</h2>
-        <button data-close-button type="button" class="close-button">&times;</button>
+modal3.insertAdjacentHTML('afterbegin', `
+    <div class='modal-header'>
+        <h2 class='title'>${allContent[0].name}</h2>
+        <button data-close-button type='button' class='close-button'>&times;</button>
     </div>
-    <ul class="sub-title">
+    <ul class='sub-title'>
         <li>${allContent[3].work[0]}</li>
         <li>${allContent[3].work[1]}</li>
         <li>${allContent[3].work[2]}</li>
     </ul>
-    <div class="modal-photo" id="photoP">
+    <div class='modal-photo' id='photoP'>
     </div>
-    <section class="modal-body">
-        <p class="modal-body1">${allContent[3].content}</p>
-        <div class="modal-body2">
-            <ul class="category">
-                <li><a href="index.html">${allContent[3].languages[0]}</a></li>
-                <li><a href="index.html">${allContent[3].languages[1]}</a></li>
-                <li><a href="index.html">${allContent[3].languages[2]}</a></li>
+    <section class='modal-body'>
+        <p class='modal-body1'>${allContent[3].content}</p>
+        <div class='modal-body2'>
+            <ul class='category'>
+                <li><a href='index.html'>${allContent[3].languages[0]}</a></li>
+                <li><a href='index.html'>${allContent[3].languages[1]}</a></li>
+                <li><a href='index.html'>${allContent[3].languages[2]}</a></li>
             </ul>
-            <div class="extra-buttons">  
-            <div class="project"><p>See Live</p><img src="image/sm1.png"></div>
-            <div class="project"><p>See Source</p><img src="image/sm2.png"></div>
+            <div class='extra-buttons'>  
+            <div class='project'><p>See Live</p><img src='image/sm1.png'></div>
+            <div class='project'><p>See Source</p><img src='image/sm2.png'></div>
             </div>
         </div>
     </section> 
