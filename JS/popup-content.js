02971a5270e54805ab64e1d8e4dbfcce7eba5d58
Tuyishimire Eric <102757126+TuyishimireEric@users.noreply.github.com>
2022-06-23 17:15:41 +0200

    Update popup-content.js

--- JS/popup-content.js
+++ JS/popup-content.js
@@ -2,7 +2,7 @@
 
 const allContent = [
   {
-    name: 'Tonic',
+    name: 'Multi-Post Stories',
     work: ['CANOPY', 'Back End Dev', '2015'],
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     At eos qui possimus nisi ducimus delectus enim beatae natus 
@@ -12,7 +12,7 @@ const allContent = [
     languages: ['HTML', 'css', 'javaScript'],
   },
   {
-    name: 'Multi-Post Stories',
+    name: 'Tonic',
     work: ['CANOPY', 'Back End Dev', '2015'],
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     At eos qui possimus nisi ducimus delectus enim beatae natus 
@@ -43,7 +43,7 @@ const allContent = [
   },
 ];
 
-const modal0 = document.querySelector('.modal');
+const modal0 = document.querySelector('.modal0');
 
 modal0.insertAdjacentHTML(
   'afterbegin',
@@ -57,7 +57,7 @@ modal0.insertAdjacentHTML(
         <li>${allContent[0].work[1]}</li>
         <li>${allContent[0].work[2]}</li>
     </ul>
-    <div class='modal-photo' id='photoP'>
+    <div class='modal-photo' id='photo11'>
     </div>
     <section class='modal-body'>
         <p class='modal-body1'>${allContent[0].content}</p>
@@ -68,8 +68,8 @@ modal0.insertAdjacentHTML(
                 <li><a href='index.html'>${allContent[0].languages[2]}</a></li>
             </ul>
             <div class='extra-buttons'>  
-            <button class='project'><p>See Live</p><img src='image/sm1.png'></butt>
-            <button class='project'><p>See Source</p><img src='image/sm2.png'></butt>
+            <button class='project'><p>See Live</p><img src='image/sm1.png'></button>
+            <button class='project'><p>See Source</p><img src='image/sm2.png'></button>
             </div>
         </div>
     </section> 
@@ -77,47 +77,13 @@ modal0.insertAdjacentHTML(
 `,
 );
 
-const modal1 = document.querySelector('#');
+const modal1 = document.querySelector('.modal1');
 
 modal1.insertAdjacentHTML(
   'afterbegin',
   `
     <div class='modal-header'>
-        <h2 class='title'>${allContent[0].name}</h2>
-        <button data-close-button type='button' class='close-button'>&times;</button>
-    </div>
-    <ul class='sub-title'>
-        <li>${allContent[0].work[0]}</li>
-        <li>${allContent[0].work[1]}</li>
-        <li>${allContent[0].work[2]}</li>
-    </ul>
-    <div class='modal-photo' id='photoP'>
-    </div>
-    <section class='modal-body'>
-        <p class='modal-body1'>${allContent[0].content}</p>
-        <div class='modal-body2'>
-            <ul class='category'>
-                <li><a href='index.html'>${allContent[0].languages[0]}</a></li>
-                <li><a href='index.html'>${allContent[0].languages[1]}</a></li>
-                <li><a href='index.html'>${allContent[0].languages[2]}</a></li>
-            </ul>
-            <div class='extra-buttons'>  
-            <div class='project'><p>See Live</p><img src='image/sm1.png'></div>
-            <div class='project'><p>See Source</p><img src='image/sm2.png'></div>
-            </div>
-        </div>
-    </section> 
-
-`,
-);
-
-const modal2 = document.querySelector('#');
-
-modal2.insertAdjacentHTML(
-  'afterbegin',
-  `
-    <div class='modal-header'>
-        <h2 class='title'>${allContent[0].name}</h2>
+        <h2 class='title'>${allContent[1].name}</h2>
         <button data-close-button type='button' class='close-button'>&times;</button>
     </div>
     <ul class='sub-title'>
@@ -125,10 +91,44 @@ modal2.insertAdjacentHTML(
         <li>${allContent[1].work[1]}</li>
         <li>${allContent[1].work[2]}</li>
     </ul>
-    <div class='modal-photo' id='photoP'>
+    <div class='modal-photo' id='photo00'>
     </div>
     <section class='modal-body'>
-        <p class='modal-body1'>${allContent[0].content}</p>
+        <p class='modal-body1'>${allContent[1].content}</p>
+        <div class='modal-body2'>
+            <ul class='category'>
+                <li><a href='index.html'>${allContent[1].languages[0]}</a></li>
+                <li><a href='index.html'>${allContent[1].languages[1]}</a></li>
+                <li><a href='index.html'>${allContent[1].languages[2]}</a></li>
+            </ul>
+            <div class='extra-buttons'> 
+            <button class='project'><p>See Live</p><img src='image/sm1.png'></button>
+            <button class='project'><p>See Source</p><img src='image/sm2.png'></button>
+            </div>
+        </div>
+    </section> 
+
+`,
+);
+
+const modal2 = document.querySelector('modal2');
+
+modal2.insertAdjacentHTML(
+  'afterbegin',
+  `
+    <div class='modal-header'>
+        <h2 class='title'>${allContent[2].name}</h2>
+        <button data-close-button type='button' class='close-button'>&times;</button>
+    </div>
+    <ul class='sub-title'>
+        <li>${allContent[2].work[0]}</li>
+        <li>${allContent[2].work[1]}</li>
+        <li>${allContent[2].work[2]}</li>
+    </ul>
+    <div class='modal-photo' id='photo1'>
+    </div>
+    <section class='modal-body'>
+        <p class='modal-body1'>${allContent[1].content}</p>
         <div class='modal-body2'>
             <ul class='category'>
                 <li><a href='index.html'>${allContent[2].languages[0]}</a></li>
@@ -145,13 +145,13 @@ modal2.insertAdjacentHTML(
 `,
 );
 
-const modal3 = document.querySelector('#');
+const modal3 = document.querySelector(' modal3');
 
 modal3.insertAdjacentHTML(
   'afterbegin',
   `
     <div class='modal-header'>
-        <h2 class='title'>${allContent[0].name}</h2>
+        <h2 class='title'>${allContent[3].name}</h2>
         <button data-close-button type='button' class='close-button'>&times;</button>
     </div>
     <ul class='sub-title'>
@@ -159,7 +159,7 @@ modal3.insertAdjacentHTML(
         <li>${allContent[3].work[1]}</li>
         <li>${allContent[3].work[2]}</li>
     </ul>
-    <div class='modal-photo' id='photoP'>
+    <div class='modal-photo' id='photo2'>
     </div>
     <section class='modal-body'>
         <p class='modal-body1'>${allContent[3].content}</p>
