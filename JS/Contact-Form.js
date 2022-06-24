24c995ae9da6cd96be3d3ac9beecaaab86fa3cb2
Tuyishimire Eric <102757126+TuyishimireEric@users.noreply.github.com>
2022-06-24 11:30:39 +0200

    Created form validation

--- JS/Contact-Form.js
+++ JS/Contact-Form.js
@@ -1,1 +1,16 @@
-// JavaScript 
+const emailForm = document.querySelector('.e-mail');
+const popUpForm = document.querySelector('.form-popup');
+const submit = document.querySelector('.submit');
+
+emailForm.addEventListener('input', () => {
+  if (/[A-Z]/.test(emailForm.value)) {
+    popUpForm.style.display = 'block';
+  } else {
+    popUpForm.style.display = 'none';
+  }
+});
+submit.addEventListener('click', (event) => {
+  if (/[A-Z]/.test(emailForm.value)) {
+    event.preventDefault();
+  }
+}, false);
