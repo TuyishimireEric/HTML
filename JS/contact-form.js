3a65bd05496620aefd487c53fa80ee6bb6be87e4
Esmatullah Ahmadzai <98796487+eaesmat@users.noreply.github.com>
2022-06-24 20:24:34 +0430

    Add popup method

--- JS/contact-form.js
+++ JS/contact-form.js
@@ -1,12 +1,12 @@
 const emailForm = document.querySelector('#e-mail');
-const popupForm = document.querySelector('.formPopup');
+const popForm = document.querySelector('.formPopup');
 const submit = document.querySelector('#submit');
 
 submit.addEventListener('click', () => {
   if (/[A-Z]/.test(emailForm.value)) {
-    popupForm.style.display = 'flex';
+    popForm.style.display = 'flex';
   } else {
-    popupForm.style.display = 'none';
+    popForm.style.display = 'none';
   }
 });
 
